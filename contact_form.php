e7d2bc227edf27dead2fabd8561ee94559939d16
massila2522 <massilalakaf@gmail.com>
2023-07-28 23:54:31 +0100

    fixed php code

--- contact_form.php
+++ contact_form.php
@@ -1,21 +1,19 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $email = $_POST['email'];
+    $sujet = $_POST['sujet'];
+    $message = $_POST['message'];
 
-   $connection = mysqli_connect('localhost','root','','newdem');
+    $to = 'newdemenagement@Gmail.com';
+    $headers = "From: $nom $prenom <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
 
-   if(isset($_POST['envoyer'])){
-      $nom = $_POST['nom'];
-      $prenom = $_POST['prenom'];
-      $email = $_POST['email'];
-      $sujet = $_POST['sujet'];
-      $message = $_POST['message'];
-    
-      $request = " insert into contact(nom, prenom, email, sujet, message) values('$nom','$prenom','$email','$sujet', '$message') ";
-      mysqli_query($connection, $request);
-
-      header('location:contact.php'); 
-
-   }else{
-      echo 'something went wrong please try again!';
-   }
-
+    if (mail($to, $sujet, $message, $headers)) {
+        echo '<p>Email sent successfully!</p>';
+    } else {
+        echo '<p>Failed to send email. Please try again later.</p>';
+    }
+}
 ?>
--- devis_form.php
+++ devis_form.php
@@ -1,29 +1,44 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $email = $_POST['email'];
+    $telephone = $_POST['telephone'];
+    $rue = $_POST['rue'];
+    $complement = $_POST['complement'];
+    $code_postal = $_POST['code_postal'];
+    $ville = $_POST['ville'];
+    $etat = $_POST['etat'];
+    $pays = $_POST['pays'];
+    $logement = $_POST['logement'];
+    $ville_arrivee = $_POST['ville_arrivee'];
+    $date_demenagement = $_POST['date_demenagement'];
 
-   $connection = mysqli_connect('localhost','root','','newdem');
+    $to = 'newdemenagement@gmail.Com';
 
-   if(isset($_POST['envoyer'])){
-      $nom = $_POST['nom'];
-      $prenom = $_POST['prenom'];
-      $email = $_POST['email'];
-      $telephone = $_POST['telephone'];
-      $rue = $_POST['rue'];
-      $complement = $_POST['complement'];
-      $code_postal = $_POST['code_postal'];
-      $ville = $_POST['ville'];
-      $etat = $_POST['etat'];
-      $pays = $_POST['pays'];
-      $logement = $_POST['logement'];
-      $ville_arrivee = $_POST['ville_arrivee'];
-      $date_demenagement = $_POST['date_demenagement'];
+    $subject = 'New Devis Form Submission';
 
-      $request = " insert into devis(nom, prenom, email, telephone, rue, complement, code_postal, ville, etat, pays, logement, ville_arrivee, date_demenagement) values('$nom','$prenom','$email','$telephone', '$rue', '$complement', '$code_postal', '$ville', '$etat', '$pays', '$logement', '$ville_arrivee', '$date_demenagement') ";
-      mysqli_query($connection, $request);
+    $message = "Nom: $nom\n";
+    $message .= "Prénom: $prenom\n";
+    $message .= "Email: $email\n";
+    $message .= "Téléphone: $telephone\n";
+    $message .= "Rue: $rue\n";
+    $message .= "Complément: $complement\n";
+    $message .= "Code Postal: $code_postal\n";
+    $message .= "Ville: $ville\n";
+    $message .= "État/Province/Région: $etat\n";
+    $message .= "Pays: $pays\n";
+    $message .= "Type de logement: $logement\n";
+    $message .= "Ville d'arrivée: $ville_arrivee\n";
+    $message .= "Date de déménagement: $date_demenagement\n";
 
-      header('location:devis.php'); 
+    $headers = "From: $nom $prenom <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
 
-   }else{
-      echo 'something went wrong please try again!';
-   }
-
+    if (mail($to, $subject, $message, $headers)) {
+        echo '<p>Email sent successfully!</p>';
+    } else {
+        echo '<p>Failed to send email. Please try again later.</p>';
+    }
+}
 ?>
